1a261d30578044f46f014c2bed2bb40448eca786
Malik Gorgis <malik01@student.rocvantwente.nl>
2022-03-16 15:05:27 +0100

    add migation

--- CarVendor/Program.cs
+++ CarVendor/Program.cs
@@ -1,7 +1,11 @@
+using CarVendor.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<CarVendorContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
